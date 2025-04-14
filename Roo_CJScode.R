d9cedc40a79fe0ed5249f4ff7355e396fcb4ee16
Rachel Bergeron <rberg079@gmail.com>
2025-04-14 16:16:08 +0200

    Restored CJS model to standalone version, now it's been added to IPM
    ...& added lots of spaces

--- Roo_CJScode.R
+++ Roo_CJScode.R
@@ -29,7 +29,7 @@ surv <- wrangleData_surv(surv.data = "data/PromSurvivalOct24.xlsx",
                          yafs.data = "data/RSmainRB_Mar25.xlsx")
 
 # create Nimble lists
-mydata  <- list(obs = surv$obs,
+myData  <- list(obs = surv$obs,
                 state = surv$state,
                 age = surv$age,
                 ageC = surv$ageC,
@@ -38,7 +38,7 @@ mydata  <- list(obs = surv$obs,
                 veg = env$veg,
                 vegE = env$vegE)
 
-myconst <- list(nind = surv$nind,
+myConst <- list(nind = surv$nind,
                 ntimes = surv$ntimes,
                 nAge = surv$nAge,
                 noAge = surv$noAge,
@@ -60,16 +60,16 @@ myconst <- list(nind = surv$nind,
 myCode = nimbleCode({
   ##### 1. Survival ####
   # Survival function
-  for (a in 1:nAge_CJS){                               
-    for (t in 1:(ntimes-1)){
-      logit(s[a,t]) <- B.age[a] +
-        dens.hat[t]*B.dens[a] +
-        veg.hat[t]*B.veg[a] +
-        # (dens.hat[t]*veg.hat[t])*B.densVeg[a] +
-        # (veg.hat[t]/dens.hat[t])*B.vegRoo[a] +
-        gamma[t,a]
-    } #t
-  } #a
+  for (i in 1:nind){                               
+    for (t in first[i]:(last[i]-1)){
+      logit(s[i, t]) <- B.age[ageC[age[i, t]]] +
+        B.dens[ageC[age[i, t]]] * dens.hat[t] +
+        B.veg[ageC[age[i, t]]] * veg.hat[t] +
+        # B.densVeg[ageC[age[i, t]]] * (dens.hat[t] * veg.hat[t]) +
+        # B.vegRoo[ageC[age[i, t]]] * (veg.hat[t] / dens.hat[t]) +
+        gamma[t, ageC[age[i,t]]]
+    } # t
+  } # i
   
   for (t in 1:ntimes){
     dens.hat[t] ~ dnorm(dens[t], sd = densE[t])
@@ -77,86 +77,86 @@ myCode = nimbleCode({
   }
   
   for (mt in 1:nNoVeg){
-    veg[mt] <- dnorm(0,0.001)
+    veg[mt] ~ dnorm(0,0.001)
   }
   
   # Estimate missing ages
   for (i in 1:nNoAge){
-    ageM[i] ~ T(dnegbin(0.25,1.6),3,20)
-    age[noAge[i],first[noAge[i]]] <- round(ageM[i])+1
+    ageM[i] ~ T(dnegbin(0.25, 1.6), 3, 20)
+    age[noAge[i], first[noAge[i]]] <- round(ageM[i]) + 1
     for (t in (first[noAge[i]]+1):ntimes){
-      age[noAge[i],t] <- age[noAge[i],t-1]+1
-    } #t
-  } #i
+      age[noAge[i], t] <- age[noAge[i], t-1] + 1
+    } # t
+  } # i
   
   # Priors for fixed effects
   for(a in 1:nAge){
-    B.age[a] ~ dlogis(0,1)
-    B.dens[a] ~ dnorm(0,0.001)
-    B.veg[a] ~ dnorm(0,0.001)
-    # B.densVeg[a] ~ dnorm(0,0.001)
+    B.age[a] ~ dlogis(0, 1)
+    B.dens[a] ~ dnorm(0, 0.001)
+    B.veg[a] ~ dnorm(0, 0.001)
+    # B.densVeg[a] ~ dnorm(0, 0.001)
   }
   
   # Variance-Covariance matrix
   for (i in 1:nAge){
     zero[i] <- 0
-    xi[i] ~ dunif(0,2)
-  } #i
+    xi[i] ~ dunif(0, 2)
+  } # i
   
   for (t in 1:(ntimes-1)){
-    eps.raw[t,1:nAge]  ~ dmnorm(zero[1:nAge], Tau.raw[1:nAge,1:nAge])
+    eps.raw[t, 1:nAge]  ~ dmnorm(zero[1:nAge], Tau.raw[1:nAge, 1:nAge])
     for (i in 1:nAge){
-      gamma[t,i] <- xi[i] * eps.raw[t,i]
-    } #i
-  } #t
-
+      gamma[t, i] <- xi[i] * eps.raw[t,i]
+    } # i
+  } # t
+  
   # Priors for precision matrix
-  Tau.raw[1:nAge, 1:nAge] ~ dwish(W[1:nAge,1:nAge], DF)
-  Sigma.raw[1:nAge, 1:nAge] <- inverse(Tau.raw[1:nAge,1:nAge])
+  Tau.raw[1:nAge, 1:nAge] ~ dwish(W[1:nAge, 1:nAge], DF)
+  Sigma.raw[1:nAge, 1:nAge] <- inverse(Tau.raw[1:nAge, 1:nAge])
   
   # Uniform covariance matrix
   # for (a in 1:nAge){
   #   zero[a] <- 0
-  #   sd.yr[a] ~ dunif(0,5)
-  #   cov.yr[a,a] <- sd.yr[a]*sd.yr[a]
+  #   sd.yr[a] ~ dunif(0, 5)
+  #   cov.yr[a, a] <- sd.yr[a] * sd.yr[a]
   # }
   #
   # for(a in 1:(nAge-1)){
   #   for(a2 in (a+1):nAge){
-  #     cor.yr[a,a2] ~ dunif(-1,1)
-  #     cov.yr[a2,a] <- sd.yr[a] * sd.yr[a2] * cor.yr[a,a2]
-  #     cov.yr[a,a2] <- cov.yr[a2,a]
+  #     cor.yr[a, a2] ~ dunif(-1, 1)
+  #     cov.yr[a2, a] <- sd.yr[a] * sd.yr[a2] * cor.yr[a, a2]
+  #     cov.yr[a, a2] <- cov.yr[a2, a]
   #   }
-  # } #i
+  # } # i
   #
   # for (t in 1:(ntimes-1)) {
-  #   gamma[t,1:nAge]  ~ dmnorm(zero[1:nAge], cov=cov.yr[1:nAge, 1:nAge])
-  # } #t
+  #   gamma[t, 1:nAge]  ~ dmnorm(zero[1:nAge], cov = cov.yr[1:nAge, 1:nAge])
+  # } # t
   
   
   ##### 2. Observation ####
   for (t in 1:ntimes){
     for(i in 1:nind){
-      logit(p[i,t]) <- mu.p + year.p[t]
+      logit(p[i, t]) <- mu.p + year.p[t]
     }
     year.p[t] ~ dnorm(0, sd = sd.p)
   }
   
   mu.p <- log(mean.p / (1-mean.p))
-  mean.p ~ dunif(0,1)
-  sd.p ~ dunif(0,10) # try dunif(0.01,10) if sd.p causes trouble
+  mean.p ~ dunif(0, 1)
+  sd.p ~ dunif(0, 10) # try dunif(0.01, 10) if sd.p causes trouble
   
   
   ##### 3. Likelihood ####
   for (i in 1:nind){
-    for (t in (first[i] + 1):last[i]){ #TODO: Double-check that the first "first[i]" = first year in IPM
+    for (t in (first[i] + 1):last[i]){
       # State process
-      state[i,t] ~ dbern(mu1[i,t])
-      mu1[i,t] <- s[ageC[age[i,t]],t-1] * state[i,t-1]
-  
+      state[i, t] ~ dbern(mu1[i, t])
+      mu1[i, t] <- s[i, t-1] * state[i, t-1]
+      
       # Observation process
-      obs[i,t] ~ dbern(mu2[i,t])
-      mu2[i,t] <- p[i,t] * state[i,t]
+      obs[i, t] ~ dbern(mu2[i, t])
+      mu2[i, t] <- p[i, t] * state[i, t]
     } #t
   } #i
   
@@ -166,29 +166,29 @@ myCode = nimbleCode({
 ## Assemble --------------------------------------------------------------------
 
 # create Nimble function
-paraNimble <- function(seed, myCode, myconst, mydata,
+paraNimble <- function(seed, myCode, myConst, myData,
                        n.burn = 1000, n.tin = 1){
   
   library(nimble)
   library(coda)
   
-  nind = myconst$ntimes
-  ntimes = myconst$ntimes
+  nind = myConst$ntimes
+  ntimes = myConst$ntimes
   
-  age = mydata$age
-  nAge = myconst$nAge
-  noAge = myconst$noAge
-  nNoAge = myconst$nNoAge
-
-  first = myconst$first
-  last = myconst$last 
-
-  veg = mydata$veg
-  dens = mydata$dens
-  nNoVeg = myconst$nNoVeg
+  age = myData$age
+  nAge = myConst$nAge
+  noAge = myConst$noAge
+  nNoAge = myConst$nNoAge
+  
+  first = myConst$first
+  last = myConst$last 
+  
+  veg = myData$veg
+  dens = myData$dens
+  nNoVeg = myConst$nNoVeg
   
   # assign initial values
-  myinits <- function(i){
+  myInits <- function(i){
     l = list(ageM = sample(3:8, size = nNoAge, replace = T),
              
              B.age = rnorm(nAge, 0, 0.25),
@@ -207,19 +207,19 @@ paraNimble <- function(seed, myCode, myconst, mydata,
              eps.raw = matrix(rnorm((ntimes-1)*nAge, 0, 0.1),
                               ncol = nAge, nrow = (ntimes-1))
              
-             # cor.yr = diag(nAge)+0.01,
+             # cor.yr = diag(nAge) + 0.01,
              # sd.yr = runif(nAge, 0,1)
     )
-    Tau.raw = diag(nAge) + rnorm(nAge^2,0,0.1)
-    l$Tau.raw = inverse((Tau.raw + t(Tau.raw))/2) # should be Sigma.raw?
+    Tau.raw = diag(nAge) + rnorm(nAge^2, 0, 0.1)
+    l$Tau.raw = inverse((Tau.raw + t(Tau.raw))/2)
     return(l)
   }
   
   # assemble model
   myMod <- nimbleModel(code = myCode,
-                       data = mydata,
-                       constants = myconst,
-                       inits = myinits())
+                       data = myData,
+                       constants = myConst,
+                       inits = myInits())
   
   # select parameters to monitor
   vars = c('year.p', 'mean.p', 'sd.p', 'state', 'ageM',
@@ -253,8 +253,8 @@ chain_output <- parLapply(X = 1:3,
                           cl = this_cluster,
                           fun = paraNimble,
                           myCode = myCode,
-                          myconst = myconst,
-                          mydata = mydata,
+                          myconst = myConst,
+                          mydata = myData,
                           n.burn = 10000,
                           n.tin = 10)
 
