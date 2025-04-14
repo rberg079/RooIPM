36bd8b15971cc3e78b6765f2e76f49f0788f2563
ChloeRNater <chloe.nater@nina.no>
2025-04-14 13:17:13 +0200

    Included CJS model in IPM and initial value simulation code

--- ProcessMod.R
+++ ProcessMod.R
@@ -43,23 +43,130 @@ myCode = nimbleCode({
   for(t in 1:(ntimes-1)){
     b[t]        ~ dunif(0.5, 1)
     s.PY[t]     ~ dunif(0.1, 1)
-    s.YAF[t]    ~ dunif(0, 1)
-    s.SA[1,t]   ~ dunif(0.5, 1)
-    s.SA[2,t]   ~ dunif(0.5, 1)
+    s.YAF[t]    <- s[1,t]
+    s.SA[1,t]   <- s[2,t]
+    s.SA[2,t]   <- s[2,t]
     
     for(a in 3:6){ # prime-aged
-      s.AD[a,t] ~ dunif(0.6, 1)
+      s.AD[a,t] <- s[3,t]
     }
     
     for(a in 7:9){ # pre-senescent
-      s.AD[a,t] ~ dunif(0.5, 1)
+      s.AD[a,t] <- s[4,t]
     }
     
     for(a in 10:nADs){ # senescent
-      s.AD[a,t] ~ dunif(0.1, 1)
+      s.AD[a,t] <- s[5,t]
     }
     
   }
+  
+  #------------------------------------#
+  # CAPTURE-MARK RECAPTURE MODEL (CJS) #
+  #------------------------------------#
+  
+  ##### 1. Survival ####
+  # Survival function
+  for (a in 1:nAge_CJS){                               
+    for (t in 1:(ntimes-1)){
+      logit(s[a,t]) <- B.age[a] +
+        dens.hat[t]*B.dens[a] +
+        veg.hat[t]*B.veg[a] +
+        # (dens.hat[t]*veg.hat[t])*B.densVeg[a] +
+        # (veg.hat[t]/dens.hat[t])*B.vegRoo[a] +
+        gamma[t,a]
+    } #t
+  } #a
+  
+  for (t in 1:ntimes){
+    dens.hat[t] ~ dnorm(dens[t], sd = densE[t])
+    veg.hat[t] ~ dnorm(veg[t], sd = vegE[t])
+  }
+  
+  for (mt in 1:nNoVeg){
+    veg[mt] <- dnorm(0,0.001)
+  }
+  
+  # Estimate missing ages
+  for (i in 1:nNoAge){
+    ageM[i] ~ T(dnegbin(0.25,1.6),3,20)
+    age[noAge[i],first[noAge[i]]] <- round(ageM[i])+1
+    for (t in (first[noAge[i]]+1):ntimes){
+      age[noAge[i],t] <- age[noAge[i],t-1]+1
+    } #t
+  } #i
+  
+  # Priors for fixed effects
+  for(a in 1:nAge){
+    B.age[a] ~ dlogis(0,1)
+    B.dens[a] ~ dnorm(0,0.001)
+    B.veg[a] ~ dnorm(0,0.001)
+    # B.densVeg[a] ~ dnorm(0,0.001)
+  }
+  
+  # Variance-Covariance matrix
+  for (i in 1:nAge){
+    zero[i] <- 0
+    xi[i] ~ dunif(0,2)
+  } #i
+  
+  for (t in 1:(ntimes-1)){
+    eps.raw[t,1:nAge]  ~ dmnorm(zero[1:nAge], Tau.raw[1:nAge,1:nAge])
+    for (i in 1:nAge){
+      gamma[t,i] <- xi[i] * eps.raw[t,i]
+    } #i
+  } #t
+  
+  # Priors for precision matrix
+  Tau.raw[1:nAge, 1:nAge] ~ dwish(W[1:nAge,1:nAge], DF)
+  Sigma.raw[1:nAge, 1:nAge] <- inverse(Tau.raw[1:nAge,1:nAge])
+  
+  # Uniform covariance matrix
+  # for (a in 1:nAge){
+  #   zero[a] <- 0
+  #   sd.yr[a] ~ dunif(0,5)
+  #   cov.yr[a,a] <- sd.yr[a]*sd.yr[a]
+  # }
+  #
+  # for(a in 1:(nAge-1)){
+  #   for(a2 in (a+1):nAge){
+  #     cor.yr[a,a2] ~ dunif(-1,1)
+  #     cov.yr[a2,a] <- sd.yr[a] * sd.yr[a2] * cor.yr[a,a2]
+  #     cov.yr[a,a2] <- cov.yr[a2,a]
+  #   }
+  # } #i
+  #
+  # for (t in 1:(ntimes-1)) {
+  #   gamma[t,1:nAge]  ~ dmnorm(zero[1:nAge], cov=cov.yr[1:nAge, 1:nAge])
+  # } #t
+  
+  
+  ##### 2. Observation ####
+  for (t in 1:ntimes){
+    for(i in 1:nind){
+      logit(p[i,t]) <- mu.p + year.p[t]
+    }
+    year.p[t] ~ dnorm(0, sd = sd.p)
+  }
+  
+  mu.p <- log(mean.p / (1-mean.p))
+  mean.p ~ dunif(0,1)
+  sd.p ~ dunif(0,10) # try dunif(0.01,10) if sd.p causes trouble
+  
+  
+  ##### 3. Likelihood ####
+  for (i in 1:nind){
+    for (t in (first[i] + 1):last[i]){ #TODO: Double-check that the first "first[i]" = first year in IPM
+      # State process
+      state[i,t] ~ dbern(mu1[i,t])
+      mu1[i,t] <- s[ageC[age[i,t]],t-1] * state[i,t-1]
+      
+      # Observation process
+      obs[i,t] ~ dbern(mu2[i,t])
+      mu2[i,t] <- p[i,t] * state[i,t]
+    } #t
+  } #i
+  
 }) # nimbleCode
 
 
--- simulateInits.R
+++ simulateInits.R
@@ -8,10 +8,68 @@
 #'
 #' @examples
 
-simulateInits <- function(ntimes = 17, nADs = 22){
+simulateInits <- function(ntimes = 17, nADs = 22, nAge_CJS = 5){
+  
+  ## Simulate true environmental conditions
+  ##---------------------------------------
+  veg.hat <- ifelse(is.na(veg), rnorm(length(veg), 0, .1), veg)
+  dens.hat <- ifelse(is.na(dens), rnorm(length(dens), 0, .1), dens)
   
   
-  ## Simulate vital rates ------------------------------------------------------
+  ## Simulate vital rate hyperparameters
+  ##------------------------------------
+  
+  # Covariate effects on survival
+  B.age <- rnorm(nAge_CJS, 0, 0.25)
+  B.veg <- rnorm(nAge_CJS, 0, 0.5)
+  B.dens <- rnorm(nAge_CJS, 0, 1)
+  #B.densVeg <- rnorm(nAge_CJS, 0, 1)
+           
+  
+  ## Simulate vital rate random effects
+  
+  # Correlated age-year REs on survival (CJS)
+  
+  # Variance-Covariance matrix
+  xi <- rnorm(nAge_CJS, 1, 0.1)
+  zero <- rep(0, nAge_CJS)
+  
+  eps.raw <- matrix(rnorm((ntimes-1)*nAge_CJS, 0, 0.1),
+                    ncol = nAge_CJS, nrow = (ntimes-1))
+  
+  # cor.yr <- diag(nAge_CJS)+0.01
+  # sd.yr <- runif(nAge_CJS, 0,1)
+  
+  for (t in 1:(ntimes-1)){
+    for (a in 1:nAge_CJS){
+      gamma[t,a] <- xi[a] * eps.raw[t,a]
+    } #a
+  } #t
+  
+  # Priors for precision matrix
+  Tau.raw <- diag(nAge_CJS) + rnorm(nAge_CJS^2,0,0.1)
+  Tau.raw <- inverse((Tau.raw + t(Tau.raw))/2)
+  Sigma.raw <- inverse(Tau.raw)
+
+  
+  
+  ## Simulate yearly vital rates ------------------------------------------------------
+  ##----------------------------
+  
+  # Age class-specific survival rates (CJS model)
+  s <- matrix(NA, nrow = nAge_CJS, ncol = ntimes-1)
+  
+  for(a in 1:nAge_CJS){
+    for(t in 1:(ntimes-1)){
+      logit(s[a,t]) <- B.age[a] +
+        dens.hat[t]*B.dens[a] +
+        veg.hat[t]*B.veg[a] +
+        # (dens.hat[t]*veg.hat[t])*B.densVeg[a] +
+        # (veg.hat[t]/dens.hat[t])*B.vegRoo[a] +
+        gamma[t,a]
+    }
+  }
+
   
   # breeding rate
   # b.exp <- mean(rs$Repro, na.rm = T) # spans 0.58-0.92
@@ -28,19 +86,37 @@ simulateInits <- function(ntimes = 17, nADs = 22){
   # to 2nd Sept 1 when they become SA1s
   # 1st age class considered in our published CJS model
   # sYAF.exp <- mean(rs$SurvSep2, na.rm = T) # spans 0.01-0.88
-  s.YAF <- runif(ntimes, 0, 1)
+  #s.YAF <- runif(ntimes, 0, 1)
+  s.YAF <- s[1, 1:(ntimes-1)]
   
   # survival of SA1s to SA2 & SA2 to AD3
   # 2nd age class in our published CJS model
-  s.SA <- matrix(runif(2 * (ntimes), 0.5, 1), nrow = 2)
+  #s.SA <- matrix(runif(2 * (ntimes), 0.5, 1), nrow = 2)
+  s.SA <- rbind(s[2, 1:(ntimes-1)], s[2, 1:(ntimes-1)])
   
   # survival of all ADs
-  s.AD            <- matrix(0, nrow = nADs, ncol = ntimes)          # 0s for AD1 & AD2
-  s.AD[3:6, ]     <- matrix(runif(4 * (ntimes), 0.6, 1), nrow = 4)  # Prime-age
-  s.AD[7:9, ]     <- matrix(runif(3 * (ntimes), 0.5, 1), nrow = 3)  # Pre-senescent
-  s.AD[10:nADs, ] <- matrix(runif((nADs - 9) * (ntimes), 0.1, 1),   # Senescent
-                            nrow = nADs - 9)
+  s.AD            <- matrix(0, nrow = nADs, ncol = ntimes-1)          # 0s for AD1 & AD2
   
+  for(a in 3:6){
+    s.AD[a, 1:(ntimes-1)]     <- s[3, 1:(ntimes-1)]  # Prime-age
+  }
+  
+  for(a in 7:9){
+    s.AD[7:9, 1:(ntimes-1)]   <- s[4, 1:(ntimes-1)]  # Pre-senescent
+  }
+  
+  for(a in 10:nADs){
+    s.AD[10:nADs, ] <- s[5, 1:(ntimes-1)]
+  }
+  
+  
+  ## Simulate observation parameters
+  ##--------------------------------
+  
+  # Recapture probabilities
+  mean.p <- runif(1, 0.6, 1)
+  year.p <- rnorm(ntimes, 0, 0.2)
+  sd.p <- rnorm(1, 0.2, 0.1)
   
   ## Simulate initial population sizes -----------------------------------------
   
@@ -58,6 +134,14 @@ simulateInits <- function(ntimes = 17, nADs = 22){
   Ntot   <- c(YAF[1] + sum(SA[1:2,1]) + sum(AD[3:nADs,1]),
               rep(NA, times = ntimes-1))
   
+  
+  ## Simulate latent states for input data
+  
+  # Unobserved ages (CJS)
+  ageM <- sample(3:8, size = nNoAge, replace = T)
+  
+  # CJS latent states (mu1, mu2)
+  # --> Not simulated so far. We may have to later. 
   
   ## Assemble myinits list -----------------------------------------------------
   
