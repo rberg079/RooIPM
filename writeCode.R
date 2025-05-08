26de0d15f9c83680beaf688a939f31887932307d
Rachel Bergeron <rberg079@gmail.com>
2025-05-08 09:06:24 +0200

    Ran longer, full IPM model (>100000) & fully age-dependent CJS

--- writeCode.R
+++ writeCode.R
@@ -116,10 +116,10 @@ writeCode <- function(){
       for(m in 1:nNoWin){
         win[m] ~ dnorm(0, sd = 2)
       }
-    }
-    
-    for(p in 1:nNoProp){
-      propF[p] ~ T(dnorm(0.8, 0.2), 0, 1)
+      
+      for(p in 1:nNoProp){
+        propF[p] ~ T(dnorm(0.8, 0.2), 0, 1)
+      }
     }
     
     
