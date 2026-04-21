0b90c1bf178f157cb093e91681a61d8444788feb
Rachel Bergeron <rberg079@gmail.com>
2026-04-21 15:23:55 -0400

    Added script to & created figures with lambda through time

--- plotResults.R
+++ plotResults.R
@@ -1,7 +1,6 @@
 # 20 April 2026
 # Plot results of IPM
 
-
 ## Set up ----------------------------------------------------------------------
 
 library(tidyverse)
@@ -58,11 +57,15 @@ pop <- df %>%
   ggplot(aes(x = Year, y = Mean)) +
   geom_ribbon(aes(ymin = Lower, ymax = Upper), fill = "#C398B7", alpha = 0.4) +
   geom_line(color = "#673C5B", linewidth = 0.8) +
-  scale_x_continuous(limits = c(2008, 2024),
-                     breaks = c(2008, 2010, 2012, 2014, 2016, 2018, 2020, 2022, 2024)) +
+  scale_x_continuous(limits = c(2009, 2024),
+                     breaks = c(2010, 2012, 2014, 2016, 2018, 2020, 2022, 2024)) +
   scale_y_continuous(breaks = pretty_breaks()) +
-  labs(y = "Female population size") +
-  theme_bw(); pop
+  labs(y = "Population size") +
+  theme_bw() +
+  theme(axis.title.x = element_blank(),
+        axis.text.x  = element_blank(),
+        axis.ticks.x = element_blank(),
+        legend.position = "none"); pop
 
 # ggsave("figures/results12ageCs/nTOT.jpeg", width = 18.0, height = 10.0, units = c("cm"), dpi = 600)
 
@@ -284,4 +287,43 @@ library(patchwork)
   theme(legend.position = "right")
 
 # ggsave("figures/results12ageCs/surv&rout&covs.jpeg", width = 18.0, height = 22.0, units = c("cm"), dpi = 600)
+
+
+## Lambda through time ---------------------------------------------------------
+
+# load results
+paramSamples <- readRDS('results/paramSamples.rds')
+
+# extract lambda
+lambda_mat <- paramSamples$t$lambda
+lambda_vec <- as.vector(lambda_mat)
+
+# summary stats
+mean(lambda_vec)
+quantile(lambda_vec, c(0.025, 0.5, 0.975))
+
+# build summary dataframe
+df <- data.frame(Year  = 1:16,
+                 Mean  = apply(lambda_mat, 2, mean),
+                 Lower = apply(lambda_mat, 2, quantile, 0.025),
+                 Upper = apply(lambda_mat, 2, quantile, 0.975))
+
+lambda <- df %>%
+  mutate(Year = Year + 2007) %>%
+  ggplot(aes(x = Year, y = Mean)) +
+  geom_ribbon(aes(ymin = Lower, ymax = Upper), alpha = 0.4, fill = "#B6967C") +
+  geom_line(colour = "#684F3B", linewidth = 0.8) +
+  geom_hline(yintercept = 1, colour = "grey40") +
+  scale_x_continuous(limits = c(2009, 2024),
+                     breaks = c(2010, 2012, 2014, 2016, 2018, 2020, 2022, 2024)) +
+  labs(x = "Year", y = expression("Population growth" ~ (lambda))) +
+  theme_bw(); lambda
+
+# ggsave("figures/results12ageCs/lambda.jpeg", width = 18.0, height = 10.0, units = c("cm"), dpi = 600)
+
+# combine with pop size
+library(patchwork)
+pop / lambda
+
+# ggsave("figures/results12ageCs/pop&lambda.jpeg", width = 18.0, height = 18.0, units = c("cm"), dpi = 600)
 
